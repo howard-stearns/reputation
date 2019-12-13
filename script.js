53b287d757f8bd6f00431b389f126a02be6727a2
howard-stearns <howard@highfidelity.io>
2019-12-12 21:13:23 -0800

    settings

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 'use strict';
 /* 
 TODO:
-- Four dead people to choose from
-- Always show the nearest online: max of either 16 or within your accuracy (no slider)
+- date of picture change
+- lower res pictures, and jpeg?
 
+- Always show the nearest online: max of either 16 or within your accuracy (no slider)
 - online/offline w/position
 - filter picker display by n|distance
 
+- endorse pick/display
+- share contact info
+
 - how do we set weightFactor? iterate?
-
-- settings: pick a picture, set initial
-- date of picture change
-- first-use: settings
-- pick your own three words
-
 
 - add to homescreen prompt 
   (https://stackoverflow.com/questions/47559051/ios-devices-add-website-to-homescreen-as-web-app-not-shortcut
@@ -21,16 +19,19 @@ TODO:
    https://stackoverflow.com/questions/50332119/is-it-possible-to-make-an-in-app-button-that-triggers-the-pwa-add-to-home-scree/50356149#50356149)
 
 - QR code
+- better picture of eleanor
+- setup cleanup
 
 => Good enough to be criticized?
 
-- share contact info
 - share interaction preferences
-- first-use: tour
 - pending words: collect for voting; secret password to show upvoted words and approve them
 - rate this app
 - lockout time (against revoting)
 - public figure of the day
+
+TBD:
+- Is there a way to make voting more anonymous by messing with the time at which it shows up - while still staying current enough?
 */
 
 // For this prototype, instead of an application-specific server that gives you just the data you need (of the potential billion users),
@@ -40,7 +41,7 @@ TODO:
 
 const Q = Croquet.Constants; // Shared among all participants, and part of the hashed definition to be replicated.
 
-Q.APP_VERSION = "RateMe 0.0.13"; // Rev'ing guarantees a fresh model (e.g., when view usage changes incompatibly during development).
+Q.APP_VERSION = "KnowMe 0.0.21"; // Rev'ing guarantees a fresh model (e.g., when view usage changes incompatibly during development).
 
 // Just used in initializing the userverse. Change this constant, and you've fractured the userverse into old and new sets!
 Q.INITIAL_WORD_LIST = `considerate courteous courageous adventurous
@@ -76,7 +77,7 @@ class UserverseModel extends WordCountModel {
             {name: "Alan Turing", words: "curious logical resourceful knowledgeable persistent kind practical rational",
              photo: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE5NDg0MDU1MTUzMTE2Njg3/alan-turing-9512017-1-402.jpg"},
             {name: "Oscar Wilde", words: "historian witty gregarious naturalist charming talented exuberant ernest adventurous",
-             photo: "https://www.biography.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_1240/MTI1MjM3OTAwMDM1MDA0ODk0/oscar-wildejpg.webp"},
+             photo: "https://www.onthisday.com/images/people/oscar-wilde-medium.jpg"},
             {name: "Cleopatra", words: "persuasive ambitious patriotic resourceful adaptable knowledgeable passionate exciting courageous adventurous persistent witty skilled",
              photo: "https://patch.com/img/cdn/users/21124/2013/01/raw/76a17acb3967536fb4f87fb31c0be86b.jpg?width=725"},
             {name: "Eleanor Roosevelt", words: "kind compassionate spiritual resourceful persuasive knowledgeable courageous",
@@ -86,7 +87,7 @@ class UserverseModel extends WordCountModel {
         this.subscribe(this.sessionId, 'ensureUserId', this.ensureUserId);
     }
     ensureUserId(userId) {
-        // Depending on when snapshot is taken, reload can give us a second ensureUserId message. Make sure it's not in the model.
+        // Depending on when snapshot is taken, reload can give us a second ensureUserId message. Make sure it's in the model.
         console.log('ensureUserId', userId, 'among', this.users);
         var userModel = this.users.find(user => user.userId === userId);
         if (userModel) return;
@@ -101,14 +102,21 @@ class UserModel extends WordCountModel { // Each user's data
     init(options) { // Initial random demo data.
         console.log('init UserModel', options, this);
         super.init(options);
-        if (options.userId) this.initRandom(options);
-        else if (options.name) this.initDeadPerson(options);
+        if (options.name) {
+            this.initDeadPerson(options);  // Name at init time only happens with the sample users.
+        } else {
+            //this.initRandom(options);
+            this.userId = options.userId;
+        }
         this.subscribe(this.userId, 'rate', this.rate);
+        this.subscribe(this.userId, 'contact', this.setContact);
+        this.subscribe(this.userId, 'photo', this.setPhoto);
+        this.subscribe(this.userId, 'threeWords', this.setThreeWords);
     }
     initDeadPerson({name, words, photo}) { // Set up as an always "online" person to play with.
         this.userId = name;
-        this.initial = name[0];
-        this.photo = photo;
+        this.setContact({name: name});
+        this.setPhoto(photo);
         words.split(/\s+/).reverse().forEach((word, index) => this.incrementWordCount(word, index + 1));
     }
     initRandom({userId}) { // For demo purposes, start the suer with random data.
@@ -137,15 +145,42 @@ class UserModel extends WordCountModel { // Each user's data
         (Math.random() * (max - min) + min).toFixed() + '%)';
     }
 
+    incrementWordCount(word, increment = 1) {
+        super.incrementWordCount(word, increment);
+        this.wellKnownModel('modelRoot').incrementWordCount(word, increment);
+    }
     rate({word, increment = 1}) {
         this.incrementWordCount(word, increment);
     }
-    incrementWordCount(word, increment = 1) {
-        // FIXME: keep a local copy of the pending net change (one word incremented by 1) and
-        // submit that some random time later, to keep feedback anonymous. But for
-        // now for demo purposes, it's nice to see the change.
-        super.incrementWordCount(word, increment);
-        this.wellKnownModel('modelRoot').incrementWordCount(word, increment);
+    setPhoto(url) {
+        console.log('setPhoto', url, this);
+        this.photo = url;
+        // FIXME set date
+        // FIXME: decide where to check against changes. here or view?
+        this.publish(this.userId, 'updateDisplay');
+    }
+    setContact({name}) {
+        console.log('setContact', name, this);
+        this.contactName = name;
+        this.initial = name[0];
+        // FIXME more
+        this.publish(this.userId, 'updateDisplay');
+    }
+    setThreeWords([word1, word2, word3]) {
+        console.log('setThreeWords', word1, word2, word3);
+        this.maybeUpdateWord(word1, 'word1', 2);
+        this.maybeUpdateWord(word2, 'word2', 1);
+        this.maybeUpdateWord(word3, 'word3', 1);
+        this.publish(this.userId, 'updateDisplay');
+    }
+    maybeUpdateWord(newWord, key, weight = 1) {
+        const oldWord = this[key];
+        console.log('maybeUpdateWord', newWord, key, oldWord, weight);
+        if (newWord === oldWord) return false;
+        if (oldWord) this.incrementWordCount(oldWord, 0 - weight);
+        if (newWord) this.incrementWordCount(newWord, weight);
+        this[key] = newWord;
+        return true;
     }
 }
 
@@ -158,33 +193,125 @@ class UserverseView extends Croquet.View { // Local version for display.
         this.subscribe(this.sessionId, "view-join", this.requestIdCheck);
         this.subscribe(this.sessionId, 'announceNewUser', this.announceNewUser);
         this.displayNearby();
+        
+        this.introScreens = ['none', 'intro', 'info', 'infoSettings', 'contact', 'selfie', 'threeWords'];
+        [goSettings].concat(Array.from(document.querySelectorAll(".next"))).forEach(button => button.onclick = () => this.nextIntroScreen());
+        Array.from(document.querySelectorAll(".back")).forEach(button => button.onclick = () => this.previousIntroScreen());
+
+        takeSelfie.onclick = () => this.takeSelfie();
+        retakeSelfie.onclick = () => this.setupSelfie();
+        acceptSelfie.onclick = () => this.acceptSelfie();
+    }
+    ensureLocalId() { // What is my locally stored anonymous id?
+        const idKey = document.title + 'UserId';
+        // Note: Even when bootstrapping, there is a zero user with no local storage.
+        return localStorage.getItem(idKey) || (localStorage.setItem(idKey, this.model.users.length), localStorage.getItem(idKey));
     }
     requestIdCheck(viewId) {
         console.log('requestIdCheck', viewId, this.viewId, Q.APP_VERSION);
         if (viewId !== this.viewId) return; // Not for us to act on.
-        const idKey = document.title + 'UserId',
-              // Note: Even when bootstrapping, there is a zero user with no local storage.
-              userId = localStorage.getItem(idKey) || (localStorage.setItem(idKey, this.model.users.length), localStorage.getItem(idKey));
-        this.publish(this.sessionId, 'ensureUserId', userId);
+        this.publish(this.sessionId, 'ensureUserId', this.ensureLocalId());
+    }
+    ensureMe() {
+        if (this.me) return this.me;
+        const userId = this.ensureLocalId();
+        this.me = this.users.find(user => user.model.userId === userId);
+        if (this.me) return this.me;
+        throw new Error(`No user view found for ${userId}.`);
     }
     announceNewUser(userId) {
-        console.log('announceNewUser', userId);
-        /*
         for (let index = this.model.users.length; index > 0;) {
             let userModel = this.model.users[--index],
                 match = userModel.userId === userId;
-            if (match) {
-                this.latest = new UserView(userModel);  // FIXME
-            }
-        }*/
+            if (!match) continue;
+            console.log('announceNewUser', userId, 'photo:', userModel.photo);
+            this.me = new UserView(userModel);
+            this.users.push(this.me)
+            nearby.append(this.me.avatar);
+            if (!userModel.initial || !userModel.photo) this.nextIntroScreen();
+            return;
+        }
     }
     computeNearby() {
-        return this.users; // FIXME: filter by online, then by sorted n|distance
+        return this.users; // FIXME: filter by online, sort by distance, filter by max/accuracy
     }
     displayNearby() {
         nearby.innerHTML = '';
         this.computeNearby().forEach(user => nearby.append(user.avatar));
     }
+    setupForVisible(screen) {
+        const me = this.ensureMe();
+        switch (screen) {
+        case 'contact':
+            contactName.value = me.model.contactName || '';
+            break;
+        case 'selfie':
+            if (me.model.photo) {
+                this.initConfirmSelfie(me.model.photo);
+            } else {
+                this.setupSelfie();
+            }
+            break;
+        case 'threeWords':
+            word1.value = me.model.word1 || '';
+            word2.value = me.model.word2 || '';
+            word3.value = me.model.word2 || '';        
+            break;
+        }
+    }
+    acceptLooseVisibility(screen) {
+        const me = this.ensureMe();
+        switch (screen) {
+        case 'contact':
+            this.publish(me.model.userId, 'contact', {name: contactName.value});
+            break;
+        case 'threeWords':
+            this.publish(me.model.userId, 'threeWords', [word1.value, word2.value, word3.value]);
+            break;
+        }
+    }
+    nextIntroScreen(increment = 1) {
+        const current = setup.className,
+              index = this.introScreens.indexOf(current),
+              nextIndex = (index + increment) % this.introScreens.length,
+              next = this.introScreens[nextIndex];
+        console.log('nextIntroScreen', current, index, nextIndex, next, this);
+        this.acceptLooseVisibility(current);
+        this.setupForVisible(next);
+        setup.className = next;
+    }
+    previousIntroScreen() {
+        this.nextIntroScreen(-1);
+    }
+    initConfirmSelfie(url) {
+        console.log('image data url', url);
+        selfieImg.setAttribute('src', url);
+        selfie.classList.remove('lineup');
+        takeSelfie.disabled = true;
+        retakeSelfie.disabled = acceptSelfie.disabled = false;
+    }
+    setupSelfie() {
+        selfie.classList.add('lineup');
+        takeSelfie.disabled = false;
+        retakeSelfie.disabled = acceptSelfie.disabled = true;
+        navigator
+            .mediaDevices
+            .getUserMedia({ video: true })
+            .then(stream => selfieVideo.srcObject = stream);
+    }
+    takeSelfie() {
+        const width = selfieVideo.videoWidth,
+              height = selfieVideo.videoHeight,
+              context = selfieCanvas.getContext('2d');
+        selfieCanvas.width = width;
+        selfieCanvas.height = height;
+        context.drawImage(selfieVideo, 0, 0, width, height);
+        this.initConfirmSelfie(selfieCanvas.toDataURL('image/png'));
+        selfieVideo.srcObject.getTracks().forEach(track => track.stop());
+    }
+    acceptSelfie() {
+        this.publish(this.me.model.userId, 'photo', selfieImg.getAttribute('src'));
+    }        
 }
 
 class UserView extends Croquet.View {
@@ -193,11 +320,15 @@ class UserView extends Croquet.View {
         this.model = model;
         const avatar = document.importNode(avatarTemplate.content.firstElementChild, true);
         this.avatar = avatar;
+        this.updateDisplay(avatar, model);
+        avatar.onclick = () => this.toggleSelection();
+        this.subscribe(model.userId, 'updateDisplay', this.updateDisplay);
+    }
+    updateDisplay(avatar = this.avatar, model = this.model) {
         avatar.querySelector('span').textContent = this.model.initial;
         const img = avatar.querySelector('img');
         if (model.photo) img.src = model.photo;
         else if (model.color) img.style.backgroundColor = model.color;
-        avatar.onclick = () => this.toggleSelection();
     }
     toggleSelection() {
         const target = this.avatar, parent = target.parentElement;
@@ -302,8 +433,8 @@ class UserView extends Croquet.View {
                     }
                 }
                 if (!user.eachCloudSpan(clickSpanIfFound)) {
-                    user.model.incrementWordCount(word);
-                    user.renderCloud(word);
+                    this.publish(this.model.userId, 'rate', {word: word});
+                    user.renderCloud(word); // FIXME- after round trip
                 }
             }
         });
